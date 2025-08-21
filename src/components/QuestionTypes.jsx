e44c55628f114fbaa6ccd5335ce664effac4c4c3
yosra zoug <yosryosra35@gmail.com>
2025-08-21 01:31:53 +0100

    useRef never used

--- src/components/QuestionTypes.jsx
+++ src/components/QuestionTypes.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect,useRef } from 'react';
+import { useState, useEffect} from 'react';
 import { CheckOutlined, CloseOutlined, DragOutlined, CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 
 // Composant True/False Question
